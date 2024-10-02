690f3878a7864e122c8db32061ae1ddac77e586c
Mootez <mootezaouinti1@gmail.com>
2024-10-02 18:28:21 +0200

    generate Doc with JsDoc

--- frontend/src/app/docs/taskDocs.js
+++ frontend/src/app/docs/taskDocs.js
@@ -1,24 +1,24 @@
-// src/app/docs/taskDocs.js
+
 
 /**
- * Represents a task in the task management system.
+ * 
  * 
  * @typedef {Object} Task
- * @property {number} [id] - The unique identifier for the task (optional).
- * @property {string} title - The title of the task.
- * @property {string} description - A brief description of the task.
- * @property {string} status - The current status of the task (e.g., 'pending', 'completed').
- * @property {Date} date - The date when the task was created or updated.
+ * @property {number} [id]
+ * @property {string} title
+ * @property {string} description
+ * @property {string} status
+ * @property {Date} date
  */
 
 /**
  * Represents a subtask associated with a task in the task management system.
  * 
  * @typedef {Object} Subtask
- * @property {number} [id] - The unique identifier for the subtask (optional).
- * @property {number} taskId - The ID of the parent task.
- * @property {string} title - The title of the subtask.
- * @property {string} description - A brief description of the subtask.
- * @property {string} status - The current status of the subtask (e.g., 'pending', 'completed').
- * @property {Date} date - The date when the subtask was created or updated.
+ * @property {number} [id]
+ * @property {number} taskId
+ * @property {string} title
+ * @property {string} description
+ * @property {string} status
+ * @property {Date} date 
  */
